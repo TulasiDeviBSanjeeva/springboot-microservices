13baecbb6117796746a7893abff0980cca95ee72
Tulasi Devi <tulasidevi@macbook-pro.localdomain>
2020-09-13 19:48:07 +0200

    Initial Commit

---
+++ movie-catalog-service/src/test/java/me/tsanjeeva/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tsanjeeva.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ratings-data-microservice/src/test/java/me/tsanjeeva/ratings_data_microservice/RatingsDataMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tsanjeeva.ratings_data_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
